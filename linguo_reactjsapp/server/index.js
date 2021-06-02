08c8fe45940ba1073ddc318039e8cc80bef9a801
Batoch <baptiste.massardier@gmail.com>
2021-06-02 14:45:31 +0200

    add dev and build conf

--- linguo_reactjsapp/server/index.js
+++ linguo_reactjsapp/server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const app = express(),
+      bodyParser = require("body-parser");
 var APIRouter = require("./routes/api");
 
 app.use("/api", APIRouter);
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
